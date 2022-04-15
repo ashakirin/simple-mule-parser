e2ebad98e2227fd715e02b35d00faa4a01160f52
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-04-15 23:37:04 +0200

    master commit1

--- src/main/resources/java-templates/route-template.java
+++ src/main/resources/java-templates/route-template.java
@@ -10,7 +10,7 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.transformer.ObjectToStringTransformer;
 
 // dsl
-// m1 m2 m3 m4 m5
+// m1 m2 m3 m4 m5 m6
 @Configuration
 public class JavaDSLAmqp {
     @Bean
