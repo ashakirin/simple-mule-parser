aea8f0cf0179a385f695396f6cd19ab6b2e7444a
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-04-15 22:33:57 +0200

    commit m1

--- src/main/java/com/vmware/sbm/mule/parser/SimpleMuleParser.java
+++ src/main/java/com/vmware/sbm/mule/parser/SimpleMuleParser.java
@@ -8,7 +8,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 
-// Simple parser for mule routes and configs
+// Simple parser for mule routes and
 public class SimpleMuleParser {
 
     public Document parseMuleConfigurationAsDom(InputStream resourceAsStream) {
