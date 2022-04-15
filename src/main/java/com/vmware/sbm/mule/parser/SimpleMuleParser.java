8c6fd6e217de2c0b505bc7949281e177b806d88f
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-04-16 00:05:26 +0200

    feature commit3

--- src/main/java/com/vmware/sbm/mule/parser/SimpleMuleParser.java
+++ src/main/java/com/vmware/sbm/mule/parser/SimpleMuleParser.java
@@ -8,7 +8,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 
-// Simple 1
+// Simple 1 2
 public class SimpleMuleParser {
 
     public Document parseMuleConfigurationAsDom(InputStream resourceAsStream) {
